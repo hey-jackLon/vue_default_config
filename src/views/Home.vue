a1c449112b122312e74d895348df9959b1c849d4
unknown <jacklon_66@163.com>
2020-06-29 09:48:55 +0800

    master

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(process.env.NODE_ENV);
+  },
+};
+</script>
+<style scoped lang='sass'>
+
+</style>
